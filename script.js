23351ed89881b0a384214036c73b27630e079dae
Lara Kaluga <9109107968@rambler.ru>
2022-03-27 21:37:38 +0300

    урок №2

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-alert("Hello my script");
-console.log("Hello JS!!!");
-let titleNew;
-let screensNew;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let titleNew = "ПРОЕКТ";
+console.log(titleNew);
+
+let screensNew = "Простые, Сложные, Интерактивные";
+console.log(screensNew.length);
+console.log(screensNew.split(" "));
+console.log(screensNew.toLowerCase());
+
+let screenPrice = 1000;
+let screenPriceStr = "Стоимость вёрстки экранов " + screenPrice + " рублей";
+console.log(screenPriceStr);
+
+let rollback = 10;
+console.log(rollback);
+
+let fullPrice = 15000;
+let fullPriceStr = "Стоимость разработки сайта " + fullPrice + " рублей";
+console.log(fullPriceStr);
+
+let rollbackNew = rollback / 100;
+let returnPrice = fullPrice * rollbackNew;
+console.log(returnPrice);
+
+let adaptive = true;
+console.log(adaptive);
+
+//alert("Hello my script");
+//console.log("Hello JS!!!");
